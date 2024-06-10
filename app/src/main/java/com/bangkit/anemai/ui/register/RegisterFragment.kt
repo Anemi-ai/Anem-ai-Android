722e81ebc218cfaac1fb7bab70ec5bae14519754
yeftaTanuKuliah <yeftatanuwijayaa@gmail.com>
2024-06-11 02:50:42 +0700

    Add fragment login dan register (NOT YET INTENT)

---
+++ app/src/main/java/com/bangkit/anemai/ui/register/RegisterFragment.kt
@@ -0,0 +1,29 @@
+package com.bangkit.anemai.ui.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bangkit.anemai.R
+import com.bangkit.anemai.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment() {
+
+    private lateinit var binding: FragmentRegisterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRegisterBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
