8626865db15ebbfba1979f94a2e49d02abc53815
Rifala <arifatulfadia.1620@gmail.com>
2024-06-13 19:25:40 +0700

    refactor: welcome page appbar and layout

--- app/src/main/java/com/bangkit/anemai/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/bangkit/anemai/ui/welcome/WelcomeActivity.kt
@@ -15,8 +15,15 @@ class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
         setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.welcome_activity)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         supportActionBar?.hide()
+
     }
 }
